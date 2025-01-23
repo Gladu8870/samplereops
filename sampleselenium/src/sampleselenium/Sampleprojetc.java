35c0663fd3b81b18be651b41096f7179c1b6b724
USER <USER@DESKTOP-3JEP5L2>
2025-01-23 11:16:32 +0530

    success

---
+++ sampleselenium/src/sampleselenium/Sampleprojetc.java
@@ -0,0 +1,16 @@
+package sampleselenium;
+
+public class Sampleprojetc {
+	public static void printValues(int num, String message) {
+		System.out.println("integer val:"+ num);
+		System.out.println("String val:"+ message);
+	}
+
+	public static void main(String[] args) {
+		int num=5;
+		String text= "hai everyone";
+		printValues(num,text);
+	}
+		// TODO Auto-generated method stub
+
+	}
